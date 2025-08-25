698d71a27025b7552b88f3e36c2d48d0bc79b7b4
luansantos702-droid <luansantos702@gmail.com>
2025-08-25 05:26:01 -0300

    fix bugs login2

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,16 @@
 import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
+import cors from "cors";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
+
+app.use(cors({
+  origin: "https://cr-trainer.onrender.com",
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
