329e9622ba764bc60f6daa29fdc115597bbc62c3
fredon1301 <luansantos80113@gmail.com>
2025-10-14 04:45:38 -0300

    Producao 1.3.8 fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { LanguageProvider } from "./hooks/useLanguage";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { queryClient } from "./lib/queryClient";
 import Landing from "@/pages/Landing";
 import Home from "@/pages/Home";
@@ -43,6 +43,7 @@ function App() {
         <TooltipProvider>
           <Toaster />
           <Router />
+          <SpeedInsights />
         </TooltipProvider>
       </LanguageProvider>
     </QueryClientProvider>
